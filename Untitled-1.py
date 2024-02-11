90e4471cbac4f55a4d62f45f389ff1476ea7181f
artur <arturzxcsf@gmail.com>
2024-02-11 18:02:43 +0200

    added by_using_while files

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,9 @@
 def sum_number(number):
-    return sum(range(number + 1))
+    try:
+        result = sum(range(number + 1))
+    except TypeError:
+        result = 0
+    return result
 
 
 if __name__ == "__main__":
