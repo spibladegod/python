3b8b8b0f6250b550088d70d7300d14c3e331286e
artur <arturzxcsf@gmail.com>
2024-02-11 18:04:47 +0200

    added by_using_while files

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,16 @@
-def sum_number(number):
+def sum_number(number: int) -> int:
+    """
+    Повертає суму цілих чисел від нуля до number, якщо number - додатне ціле число, інакше повертає 0.
+
+    >>> sum_number(5)
+    15
+    >>> sum_number(5.3)
+    0
+    >>> sum_number(-5)
+    0
+    >>> sum_number("5")
+    0
+    """
     try:
         result = sum(range(number + 1))
     except TypeError:
@@ -7,4 +19,7 @@ def sum_number(number):
 
 
 if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
     print(sum_number(10))
